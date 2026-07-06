9ce5777224458e41b4692d6614841107fae0c107
Hatim <hatimlakrouni@gmail.com>
2026-07-06 16:44:25 +0100

    feat: implement PrintModule component for custom positioning and printing of bank cheques and effects

--- src/pages/PrintModule.tsx
+++ src/pages/PrintModule.tsx
@@ -485,7 +485,7 @@ export function PrintModule() {
             position: absolute;
             right: 0;
             left: auto;
-            top: 48%;
+            top: 49%;
             transform: translateY(-50%);
             margin: 0;
             padding: 0;
